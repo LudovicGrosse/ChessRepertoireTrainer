15fd87e65f2a71ae0451380207a2dfc9f0d0026b
Ludovic Grosse <lg34600@gmail.com>
2026-03-31 12:58:10 +0200

    Fix 401/403 session expiration and unauthorized access handling

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const authenticateToken = (req, res, next) => {
     if (!token) return res.sendStatus(401);
 
     jwt.verify(token, SECRET_KEY, (err, user) => {
-        if (err) return res.sendStatus(403);
+        if (err) return res.sendStatus(401);
         req.user = user;
         next();
     });
